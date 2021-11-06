4e0de23778f57d78a2edde57a08548b3be364da4
KarolCastillo <82273213+KarolCastillo@users.noreply.github.com>
2021-11-06 00:37:00 -0600

    se crea el controlador y cuatro vistas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::get("/",[\App\Http\Controllers\CriptomonedaController::class,'index']);
+//Route::get('/', function () {
+//    return view('index');
+//});
